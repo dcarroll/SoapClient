2c9e1300bc5122c25c8f4d4db086e89c424f053b
Dave Carroll <djcarroll@gmail.com>
2015-05-05 11:00:33 -0700

    add web reference

---
+++ SoapClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoapClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
